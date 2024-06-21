ecf513dce254baa85a31725470270e0c02fcd506
Anas Ahammad <anasahammad2002@gmail.com>
2024-06-21 11:08:30 +0600

    stairtransition removed

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={RobotoMono.variable}>
         <Header/>
-        <StairTransition/>
+        {/* <StairTransition/> */}
         <PageTransition>
 
         {children}
